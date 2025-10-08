f7c69555ee89f5ea62936d3cacfaffa429dcb0b3
asukachan477-spec <asukachan477@gmail.com>
2025-10-08 17:46:06 +0100

    Create lora.py

---
+++ utils/lora.py
@@ -0,0 +1,7 @@
+def get_loras():
+    # Klickbare Loras von Civitai
+    return [
+        {"name": "Cute Girl Lora", "url": "https://civitai.com/models/12345"},
+        {"name": "Anime Hair Lora", "url": "https://civitai.com/models/67890"},
+        {"name": "Fantasy Outfit Lora", "url": "https://civitai.com/models/54321"},
+    ]
